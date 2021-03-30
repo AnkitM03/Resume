1ddaf26366dce7a7493109948ecd87bbe7dc08fd
AnkitM03 <ankitm.am52@gmail.com>
2021-03-30 12:09:52 +0530

    New commands added to deploy project in heroku

--- resume/settings.py
+++ resume/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3sqxvc-0g4fmnhuv=mjzw(%b63t6puv8$h=u+8-v2%zrqry6-!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
